a54a1a65447c9851d783f888575740680bc67dd4
Poisonbeak <michele.alfano@davincimilazzo.edu.it>
2025-01-27 16:31:57 +0100

    menu loop

--- calculator.js
+++ calculator.js
@@ -5,33 +5,37 @@ const arrowFunc = require("./arrow_functions.js");
 const inlineFunc = require("./inline_functions.js")
 
 function menu(){
-    console.log(`Choose function type:
-    1. Normal functions
-    2. Expression functions
-    3. Arrow functions
-    4. Inline functions
-    5. Quit
-    `);
-    console.log();    
-    let choice = prompt();
-
-    switch (choice) {
-        case '1':
-            regFunc.calc();
-            break;
-        case '2':
-            expFunc.calc();
-            break;
-        case '3':
-            arrowFunc.calc();
-            break;
-        case '4':
-            inlineFunc.calc();
-            break;
-        case '5':
-            process.exit(1);
-        default:
-            break;
+    while (true) {
+        console.log(`Choose function type:
+        1. Normal functions
+        2. Expression functions
+        3. Arrow functions
+        4. Inline functions
+        5. Quit
+        `);
+        console.log();    
+        let choice = prompt();
+    
+        switch (choice) {
+            case '1':
+                regFunc.calc();
+                break;
+            case '2':
+                expFunc.calc();
+                break;
+            case '3':
+                arrowFunc.calc();
+                break;
+            case '4':
+                inlineFunc.calc();
+                break;
+            case '5':
+                process.exit(1);
+            default:
+                console.log("Choice not recognized.");
+                
+                break;
+        }
     }
 }
 
